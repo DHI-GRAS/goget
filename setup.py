23f6b98f13a75df54cd3e2b3c67e3d691f7460ff
Dion Häfner <dimh@dhigroup.com>
2017-10-20 13:15:54 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='goget',
+    version='0.1',
+    description='Downloads stuff, in pure Python',
+    author='Dion Häfner',
+    author_email='dimh@dhigroup.com',
+    packages=find_packages()
+    )
